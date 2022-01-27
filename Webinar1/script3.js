7601132c2ed7c24d9745242e2a3480a90a813814
Kanyshai Osmonova <osmonova.kanyshai@gmail.com>
2022-01-27 18:57:49 +0600

    added 4th tasks

--- Webinar1/script3.js
+++ Webinar1/script3.js
@@ -1,4 +1,4 @@
 //3. Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
-let userName = prompt('What is your name?');
-let userEmail = prompt("What is your email?");
-document.write(`<b>Name :</b> ${userName}, <br><b>Email: </b>${userEmail}`);
+// let userName = prompt('What is your name?');
+// let userEmail = prompt("What is your email?");
+// document.write(`<b>Name :</b> ${userName}, <br><b>Email: </b>${userEmail}`);
--- Webinar1/script4.js
+++ Webinar1/script4.js
@@ -1,1 +1,6 @@
 //4. Интернет-магазин занимается продажей различных сувениров и безделушек. Каждый сувенир весит 75 г, а безделушка – 112 г. Напишите программу, запрашивающую у пользователя количество тех и других покупок, после чего выведите на экран общий вес посылки.
+// let souvenirsWeight = 75;
+// let baublesWeight = 112;
+// let userSouvenirsWeight = prompt('Сколько штук сувениров Вы купили?')*souvenirsWeight;
+// let userBaublesWeight = prompt('Сколько безделушек Вы купили? ')*baublesWeight;
+// document.write(`ОБЩИЙ ВЕС ПОСЫЛКИ : ${(userSouvenirsWeight+userBaublesWeight)/1000}килограммов`);
