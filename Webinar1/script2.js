61a79e59a17a6d40bef6f0634d74ce70c7f74076
Kanyshai Osmonova <osmonova.kanyshai@gmail.com>
2022-01-27 18:53:18 +0600

    add the third tasks

--- Webinar1/script2.js
+++ Webinar1/script2.js
@@ -1,2 +1,5 @@
 //2. Простые сообщения: сохраните сообщение в переменной и выведите это сообщение. Затем замените значение переменной другим сообщением и выведите новое сообщение
-let message 
+// let message = 'I wish you good luck in this challenge';
+// alert(message);
+// message = 'We can do it';
+// alert(message);
