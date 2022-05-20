5e57c36f8ba087d8f1d07adf69d9f06396702570
Kanyshai Osmonova <osmonova.kanyshai@gmail.com>
2022-05-20 19:18:47 +0600

    Update script3.js

--- Webinar1/script3.js
+++ Webinar1/script3.js
@@ -1,4 +1,4 @@
 //3. Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
-// let userName = prompt('What is your name?');
-// let userEmail = prompt("What is your email?");
+// let userName = prompt('What is your favourite boysband?');
+// let userEmail = prompt("What is your favourite girlsband?");
 // document.write(`<b>Name :</b> ${userName}, <br><b>Email: </b>${userEmail}`);
